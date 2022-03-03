d83a0e918fee27d05002a7e2292588f55bdd1804
Shaikh18 <sms.shaheryar@gmail.com>
2022-03-04 04:20:14 +0500

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('type');
+            $table->string('passport_number');
+            $table->string('city');
+            $table->string('pin');
+            $table->string('last_location');
+            $table->string('violation_count')->default(0);
+            $table->dateTime('completion_date');
+            $table->dateTime('last seen');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
