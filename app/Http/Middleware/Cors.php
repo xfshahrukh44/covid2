9478e6948cac9632134d6091062cf5add8dd8826
xfshahrukh44 <xixno1@gmail.com>
2022-04-01 14:00:48 +0500

    csrf token

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,8 @@ class Cors
     {
 //        return dd('asd');
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin', csrf_token())
+            ->header('_token', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
