2b7ccce8877ed55c2fdacd256fce4a3054ce93ec
xfshahrukh44 <xixno1@gmail.com>
2022-04-05 02:14:33 +0500

    5/4/22

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
     {
 //        return dd('asd');
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+//            ->header('Access-Control-Allow-Origin', '*')
 //            ->header('_token', csrf_token())
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
 header('Access-Control-Allow-Origin', "*");
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
